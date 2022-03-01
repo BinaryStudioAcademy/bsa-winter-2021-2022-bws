18e0c1c375c616e0ea73c4a21b8dbfdc6ab91fe4
ania-leichenko <ania.leichenko@gmail.com>
2022-03-01 11:57:37 +0200

    BWS-116: + validation for the tenant name field
    save

--- shared/src/validation/validation-schemas.ts
+++ shared/src/validation/validation-schemas.ts
@@ -2,7 +2,11 @@ export {
   EamWorkerCreate,
   eamWorkerCreateBackend,
 } from './eam-worker/eam-worker';
-export { eamMasterSignUp, eamMasterSignIn } from './eam-master/eam-master';
+export {
+  eamMasterSignUp,
+  eamMasterSignIn,
+  eamWorkerSignIn,
+} from './eam-master/eam-master';
 export { eamGroupCreate, eamGroupConfigurate } from './eam-group/eam-group';
 export { bsSpaceCreate } from './bs-space/bs-space';
 export { scInstanceCreate } from './sc-instance/sc-instance';
