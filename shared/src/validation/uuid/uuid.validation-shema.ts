5c32a5f47fba4e016345328f5eb57bd20b61af8f
bohdanshcherba <73034554+bohdanshcherba@users.noreply.github.com>
2022-03-29 19:42:33 +0300

    bws-139: + some fix

--- shared/src/validation/uuid/uuid.validation-shema.ts
+++ shared/src/validation/uuid/uuid.validation-shema.ts
@@ -1,13 +1,12 @@
 import * as Joi from 'joi';
-import { getNameOf } from '~/helpers/helpers';
-import { SLCFunctionLoadParamsDto } from '~/common/types/types';
+
 import {
   UUIDValidationRule,
   UUIDValidationMessage,
 } from '~/common/enums/enums';
 
 const UUID = Joi.object({
-  [getNameOf<SLCFunctionLoadParamsDto>('id')]: Joi.string()
+  ['id']: Joi.string()
     .required()
     .regex(UUIDValidationRule.UUID_PATTERN)
     .messages({
